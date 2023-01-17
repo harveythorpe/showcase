32ac4f855e6689f1452f5c69701fcf3651bf8f32
Harvey <harveythorpe@gmail.com>
2023-01-17 16:12:59 +1100

    checkpoint

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
